c1090ebc1aed274668c915f4830006ae47a7fc09
Ramazan <laumanov.r@gmail.com>
2021-02-22 11:36:36 +0600

    create vuex for account

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,11 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { account } from "./account.module";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  modules: {
+    account
+  }
 });
