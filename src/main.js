9988c9ad7faccf58638b10595cd41ef72100577b
Ramazan <laumanov.r@gmail.com>
2021-02-22 15:26:42 +0600

    lint main-js

--- src/main.js
+++ src/main.js
@@ -4,12 +4,12 @@ import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import VModal from "vue-js-modal";
+import VueToast from "vue-toast-notification";
 import "@/assets/style/main.scss";
-import VueToast from 'vue-toast-notification';
 
 Vue.config.productionTip = false;
 Vue.use(VModal);
-Vue.use(VueToast, { position: 'top', duration: 2500 });
+Vue.use(VueToast, { position: "top", duration: 2500 });
 
 new Vue({
   router,
