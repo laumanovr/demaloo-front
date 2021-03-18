4511f561d424ef4019379042d533d50ffcd70f74
Ramazan <laumanov.r@gmail.com>
2021-03-18 12:44:25 +0600

    create auth-header

--- src/utils/authHeader.js
+++ src/utils/authHeader.js
@@ -1,8 +1,7 @@
 export const authHeader = () => {
-  const user = JSON.parse(window.localStorage.getItem("user"));
-  if (user && user.token) {
-    return { Authorization: "Bearer_" + user.token };
-  } else {
-    return {};
-  }
+	const user = JSON.parse(window.localStorage.getItem('demalooUser'));
+	if (user && user.token) {
+		return {Authorization: `Bearer ${user.token}`};
+	}
+	return {};
 };
