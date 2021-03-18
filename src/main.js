10783b31d6a8727ba080bcb2edcf2f1c99aed651
Ramazan <laumanov.r@gmail.com>
2021-03-18 12:53:52 +0600

    modified main.js

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import vuetify from "./plugins/vuetify";
-import VModal from "vue-js-modal";
-import VueToast from "vue-toast-notification";
-import "@/assets/style/main.scss";
+import Vue from 'vue';
+import VModal from 'vue-js-modal';
+import VueToast from 'vue-toast-notification';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import vuetify from './plugins/vuetify';
+import '@/assets/style/main.scss';
 
 Vue.config.productionTip = false;
+Vue.use(VueToast, {position: 'top', duration: 2500});
 Vue.use(VModal);
-Vue.use(VueToast, { position: "top", duration: 2500 });
 
 new Vue({
-  router,
-  store,
-  vuetify,
-  render: h => h(App)
-}).$mount("#app");
+	router,
+	store,
+	vuetify,
+	render: (h) => h(App),
+}).$mount('#app');
