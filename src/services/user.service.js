c8cf0911ee2c92a3d23f2f5a8a59eca9fbb9df31
Ramazan <laumanov.r@gmail.com>
2021-03-18 13:08:31 +0600

    create user service

---
+++ src/services/user.service.js
@@ -0,0 +1,42 @@
+import {API_BASE_URL, sendPostRequest, sendGetRequest, sendPatchRequest} from './api.service';
+
+export class UserService {
+	constructor() {}
+
+	static loginCompany(data) {
+		const url = `${API_BASE_URL}/companies/login`;
+		return sendPostRequest(url, data);
+	}
+
+	static registerCompany(data) {
+		const url = `${API_BASE_URL}/companies/signup`;
+		return sendPostRequest(url, data);
+	}
+
+	static fetchCompanyProfile() {
+		const url = `${API_BASE_URL}/companies/me`;
+		return sendGetRequest(url);
+	}
+
+	static updateCompanyProfile(body) {
+		const url = `${API_BASE_URL}/companies/updateMe`;
+		return sendPatchRequest(url, body);
+	}
+
+	// ---------------------------------------------------------------
+
+	static registerClient(body) {
+		const url = `${API_BASE_URL}/signup`;
+		return sendPostRequest(url, body);
+	}
+
+	static loginClient(body) {
+		const url = `${API_BASE_URL}/login`;
+		return sendPostRequest(url, body);
+	}
+
+	static updateClient(body) {
+		const url = `${API_BASE_URL}/customers/updateMe`;
+		return sendPatchRequest(url, body);
+	}
+}
