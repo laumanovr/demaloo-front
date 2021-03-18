05024ed6c004157949ba8c87324c6893d7ca44ee
Ramazan <laumanov.r@gmail.com>
2021-03-18 12:47:27 +0600

    create close-icon

---
+++ src/components/icons/CloseIcon.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="close-icon" @click="$emit('click')">
+        <img src="../../assets/icons/close.svg"/>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .close-icon {
+        display: inline-block;
+        cursor: pointer;
+        width: 26px;
+        min-width: 26px;
+        height: 26px;
+        img {
+            border-radius: 5px;
+            padding: 3px;
+            width: 100%;
+            height: 100%;
+        }
+    }
+</style>
