d27c4bc03a2a183b14428f54ae320bfc990c10c4
Ramazan <laumanov.r@gmail.com>
2021-03-18 12:49:37 +0600

    create main vuex module

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,15 @@
-import Vue from "vue";
-import Vuex from "vuex";
-import { account } from "./account.module";
+import Vue from 'vue';
+import Vuex from 'vuex';
+import {account} from './account.module';
+import {location} from './location.module';
+import {notification} from './notification.module';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  modules: {
-    account
-  }
+	modules: {
+		account,
+		location,
+		notification
+	},
 });
