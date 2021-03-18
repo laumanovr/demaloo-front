c44b793774d494cd513dda544507bfa3be447195
Ramazan <laumanov.r@gmail.com>
2021-03-18 12:59:23 +0600

    create refresh icon

---
+++ src/components/icons/RefreshIcon.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="edit-icon" @click="$emit('click')">
+    <img src="../../assets/icons/update-icon.svg" :title="iconTitle" />
+  </div>
+</template>
+
+<script>
+export default {
+	props: {
+		iconTitle: {type: String, default: 'Пересоздать'},
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+.edit-icon {
+  display: inline-block;
+  cursor: pointer;
+  margin-right: 10px;
+  width: 24px;
+  min-width: 24px;
+  height: 24px;
+  img {
+    border-radius: 5px;
+    padding: 3px;
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
