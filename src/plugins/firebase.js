cdb888ae1475a08253a7503e25d0f4e82c6bbbd5
Ramazan <laumanov.r@gmail.com>
2021-03-18 12:48:04 +0600

    create firebase config

---
+++ src/plugins/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const firebaseConfig = {
+	apiKey: "AIzaSyAPXIcotkyYkL6fvHIh7Kyf3w5b_5HdK9I",
+	authDomain: "demaloo-ae263.firebaseapp.com",
+	projectId: "demaloo-ae263",
+	storageBucket: "demaloo-ae263.appspot.com",
+	messagingSenderId: "616463014243",
+	appId: "1:616463014243:web:25485a3cf67b62ad5e9b56",
+	measurementId: "G-XLV0R3R0K9",
+};
+export default firebase.initializeApp(firebaseConfig);
