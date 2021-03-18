80525dbd2508aedf1718146db7309647914998d0
Ramazan <laumanov.r@gmail.com>
2021-03-18 12:45:42 +0600

    create client-manage component

---
+++ src/views/client/ClientManage.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="client-manage">
+		<Header/>
+		<router-view/>
+		<Footer/>
+	</div>
+</template>
+
+<script>
+import Header from '@/components/general/Header';
+import Footer from '@/components/general/Footer';
+
+export default {
+	components: {
+		Header,
+		Footer
+	}
+};
+</script>
+
+<style lang="scss">
+	.client-manage {
+		min-width: 1010px;
+	}
+</style>
