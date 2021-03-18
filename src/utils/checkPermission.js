4da0ac004edb652ad22a56e7325f3cc1a97e34b0
Ramazan <laumanov.r@gmail.com>
2021-03-18 13:26:21 +0600

    protected routes, added router guards

---
+++ src/utils/checkPermission.js
@@ -0,0 +1,16 @@
+export const isCompany = (to, from, next) => hasRole(to, next, 'company');
+
+export const isClient = (to, from, next) => hasRole(to, next, 'customer');
+
+export const isSuperAdmin = (to, from, next) => hasRole(to, next, 'superAdmin');
+
+const hasRole = (to, next, role) => {
+	if (to.meta.requireAuth) {
+		const user = JSON.parse(window.localStorage.getItem('demalooUser'));
+		if (user && user.data.user.role.includes(role)) {
+			return next();
+		}
+		return next('/login');
+	}
+	return next();
+};
