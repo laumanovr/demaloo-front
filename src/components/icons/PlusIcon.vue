da5eb554b505eb3ca219dfd297fec36267c60c4c
Ramazan <laumanov.r@gmail.com>
2021-03-18 12:56:51 +0600

    create plus-icon

---
+++ src/components/icons/PlusIcon.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="plus-icon" @click="$emit('click')">
+    <img
+      src="../../assets/icons/plus-icon.svg"
+      :alt="iconTitle"
+      :title="iconTitle"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+	props: {
+		iconTitle: String,
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+.plus-icon {
+  display: inline-block;
+  cursor: pointer;
+  margin-right: 10px;
+  width: 26px;
+  min-width: 26px;
+  height: 26px;
+  img {
+    background: $blue-primary;
+    border-radius: 5px;
+    padding: 3px;
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
