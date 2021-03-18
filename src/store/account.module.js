8aafb53690cb3dc67734ebe7516965b806763ab6
Ramazan <laumanov.r@gmail.com>
2021-03-18 12:42:41 +0600

    modified account store vuex

--- src/store/account.module.js
+++ src/store/account.module.js
@@ -1,65 +1,111 @@
-import UserService from "@/services/user.service";
-import router from "@/router";
+import {UserService} from '../services/user.service';
+import router from '@/router';
 
-const user = JSON.parse(window.localStorage.getItem("demalooUser"));
-
-const roles = [
-  {
-    role: "company",
-    url: ""
-  },
-  {
-    role: "client",
-    url: ""
-  },
-  {
-    role: "superAdmin",
-    url: ""
-  }
-];
+const profile = JSON.parse(window.localStorage.getItem('demalooUser'));
+const customer = profile && profile.data.user.role === 'customer' ? profile.data.user : {};
+const company = profile && profile.data.user.role === 'company' ? profile.data.user : {};
 
 const state = {
-  user: user ? user : {},
-  onError: ""
+	customer: customer,
+	company: company,
+	onError: '',
+	onSuccess: ''
 };
 
 const actions = {
-  login({ commit }, data) {
-    UserService.login(data)
-      .then(res => {
-        window.localStorage.setItem("user", JSON.stringify(res));
-        commit("setUser", res);
-        const foundRole = roles.find(i => i.role === res.role);
-        router.push(foundRole.url);
-      })
-      .catch(err => {
-        commit("setError", err);
-      });
-  },
+	async companyLogin({commit, dispatch}, data) {
+		try {
+			const res = await UserService.loginCompany(data);
+			dispatch('setInitialUser', res);
+			dispatch('location/fetchRegions', {}, {root: true});
+			dispatch('notification/checkCompanyNotifies', {}, {root: true});
+			router.push('/company-manage');
+		} catch (err) {
+			commit('setError', err);
+		}
+	},
 
-  registerClient() {},
+	async companyUpdate({commit, dispatch}, data) {
+		try {
+			const res = await UserService.updateCompanyProfile(data);
+			dispatch('updateProfileData', res.data.user);
+		} catch (err) {
+			commit('setError', err);
+		}
+	},
 
-  logout({ commit }) {
-    UserService.logout();
-    commit("removeUser");
-  }
+	async clientLogin({commit, dispatch}, data) {
+		try {
+			const res = await UserService.loginClient(data);
+			dispatch('setInitialUser', res);
+			if (res.data.user.name) {
+				router.push('/');
+			} else {
+				router.push('/profile-manage');
+			}
+		} catch (err) {
+			commit('setError', err);
+		}
+	},
+
+	async clientRegister({commit, dispatch}, data) {
+		try {
+			const res = await UserService.registerClient(data);
+			dispatch('setInitialUser', res);
+			router.push('/profile-manage');
+		} catch (err) {
+			commit('setError', err);
+		}
+	},
+
+	async clientUpdate({commit, dispatch}, data) {
+		try {
+			const res = await UserService.updateClient(data);
+			dispatch('updateProfileData', res.data.user);
+		} catch (err) {
+			commit('setError', err);
+		}
+	},
+
+	setInitialUser({commit}, res) {
+		commit('setUser', {role: res.data.user.role, user: res.data.user});
+		window.localStorage.setItem('demalooUser', JSON.stringify(res));
+	},
+
+	updateProfileData({commit}, updateUser) {
+		const profile = JSON.parse(window.localStorage.getItem('demalooUser'));
+		profile.data.user = updateUser;
+		window.localStorage.setItem('demalooUser', JSON.stringify(profile));
+		commit('setUser', {role: updateUser.role, user: updateUser});
+		commit('setSuccess', 'success');
+	},
+
+	logout({commit}) {
+		window.localStorage.removeItem('demalooUser');
+		commit('removeUser');
+		router.push('/login');
+	},
 };
 
 const mutations = {
-  setUser(state, data) {
-    state.user = data;
-  },
-  removeUser(state) {
-    state.user = null;
-  },
-  setError(state, error) {
-    state.onError = error;
-  }
+	setUser(state, data) {
+		state[data.role] = data.user;
+	},
+	removeUser(state) {
+		state.customer = {};
+		state.company = {};
+	},
+	setSuccess(state, success) {
+		state.onSuccess = success;
+	},
+	setError(state, error) {
+		state.onError = error;
+	},
 };
 
 export const account = {
-  namespaced: true,
-  state,
-  actions,
-  mutations
+	namespaced: true,
+	state,
+	actions,
+	mutations,
 };
