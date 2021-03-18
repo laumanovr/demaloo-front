84b5758d1c8d96f6a14e76916848f74bb22a5124
Ramazan <laumanov.r@gmail.com>
2021-03-18 13:08:59 +0600

    create tour-service

---
+++ src/services/tour.service.js
@@ -0,0 +1,75 @@
+import {
+	API_BASE_URL,
+	sendPostRequest,
+	sendGetRequest,
+	sendPatchRequest,
+	sendDeleteRequest
+} from './api.service';
+
+export class TourService {
+	constructor() {}
+
+	// -------------------------COMPANY TOURS START-----------------------
+	static createTour(data) {
+		const url = `${API_BASE_URL}/companies/tours`;
+		return sendPostRequest(url, data);
+	}
+
+	static updateTour(data) {
+		const url = `${API_BASE_URL}/companies/tours/${data._id}`;
+		return sendPatchRequest(url, data);
+	}
+
+	static fetchTourBookings(tourId) {
+		const url = `${API_BASE_URL}/companies/tours/${tourId}/bookings?limit=100`;
+		return sendGetRequest(url);
+	}
+
+	static fetchCompanyTours(queryParams='') {
+		const url = `${API_BASE_URL}/companies/tours?sort=-date${queryParams}`;
+		return sendGetRequest(url);
+	}
+
+	static fetchTourById(tourId) {
+		const url = `${API_BASE_URL}/companies/tours/${tourId}`;
+		return sendGetRequest(url);
+	}
+
+	static addImagesForTour(tourId, formData) {
+		const url = `${API_BASE_URL}/companies/tours/${tourId}/uploadImages`;
+		return sendPatchRequest(url, formData);
+	}
+
+	static addManualBooking(tourId, body) {
+		const url = `${API_BASE_URL}/companies/tours/${tourId}/bookings`;
+		return sendPostRequest(url, body);
+	}
+
+	static deleteManualBooking(bookingId) {
+		const url = `${API_BASE_URL}/companies/bookings/${bookingId}`;
+		return sendDeleteRequest(url);
+	}
+	// --------------COMPANY TOURS END------------------------------------------
+	// -----------------CLIENT TOURS START--------------------------------------
+
+	static fetchAllTours() {
+		const url = `${API_BASE_URL}/tours`;
+		return sendGetRequest(url);
+	}
+
+	static fetchClientTourDetail(tourId) {
+		const url = `${API_BASE_URL}/tours/${tourId}`;
+		return sendGetRequest(url);
+	}
+
+	static paymentForTour(body) {
+		const url = `${API_BASE_URL}/customers/bookings/purchase`;
+		return sendPostRequest(url, body);
+	}
+
+	static reservationForTour(body) {
+		const url = `${API_BASE_URL}/customers/bookings/reserve`;
+		return sendPostRequest(url, body);
+	}
+
+}
