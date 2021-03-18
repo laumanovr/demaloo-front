1ab284993b09d4756f5722bc87b0862042c1eee7
Ramazan <laumanov.r@gmail.com>
2021-03-18 12:47:39 +0600

    create edit-icon

---
+++ src/components/icons/EditIcon.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="edit-icon" @click="$emit('click')">
+    <img src="../../assets/icons/edit-icon.svg" title="Редактировать" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.edit-icon {
+  display: inline-block;
+  cursor: pointer;
+  margin-right: 10px;
+  width: 26px;
+  min-width: 26px;
+  height: 26px;
+  img {
+    border-radius: 5px;
+    padding: 3px;
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
