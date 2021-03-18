35c78a458677c180ff60236818a706380c3f4d77
Ramazan <laumanov.r@gmail.com>
2021-03-18 13:02:49 +0600

    create general sidebar object

---
+++ src/utils/sidebars.js
@@ -0,0 +1,59 @@
+export default {
+	company: [
+		{
+			name: 'Туры',
+			route: '/company-manage',
+			icon: 'dashboard.svg',
+		},
+		// {
+		// 	name: 'Клиенты',
+		// 	route: '',
+		// 	icon: 'people.svg',
+		// },
+		{
+			name: 'Отзывы',
+			route: '/company-manage/reviews',
+			icon: 'assignment.svg',
+		},
+		{
+			name: 'Уведомления',
+			route: '/company-manage/notifications',
+			icon: 'notifications.svg',
+		},
+		{
+			name: 'Чат',
+			route: '#',
+			icon: 'message.svg',
+		}
+	],
+
+	client: [
+		{
+			name: 'Профиль',
+			route: '/profile-manage',
+			icon: 'avatar.svg'
+		},
+		{
+			name: 'Мои туры',
+			route: '#',
+			icon: 'map-marker.svg'
+		},
+		{
+			name: 'Мои отзывы',
+			route: '#',
+			icon: 'review.svg'
+		},
+		{
+			name: 'Сообщения',
+			route: '#',
+			icon: 'envelope.svg'
+		},
+		{
+			name: 'Уведомления',
+			route: '#',
+			icon: 'bell.svg'
+		}
+	],
+
+	superAdmin: []
+};
