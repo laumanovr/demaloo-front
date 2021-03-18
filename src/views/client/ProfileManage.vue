d87aa657ae39da0d7a73cf9b947735e3b39186da
Ramazan <laumanov.r@gmail.com>
2021-03-18 12:59:11 +0600

    create profile-manage in client

---
+++ src/views/client/ProfileManage.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="client-profile-manage">
+		<ClientSidebar :role="'client'"/>
+		<div class="client-profile-manage__content">
+			<router-view></router-view>
+		</div>
+	</div>
+</template>
+
+<script>
+import ClientSidebar from '@/components/client/ClientSidebar';
+export default {
+	components: {
+		ClientSidebar
+	}
+};
+</script>
+
+<style lang="scss">
+.client-profile-manage {
+	display: flex;
+	width: 100%;
+	max-width: 1366px;
+	margin: 0 auto;
+	border-bottom: 1px solid $gray-light;
+	&__content {
+		padding: 30px 20px 70px 50px;
+	}
+}
+</style>
