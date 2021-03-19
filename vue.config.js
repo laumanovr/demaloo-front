526e5ab4f55d7f62a2c1ca43c8afe3ac3638f425
Ramazan <laumanov.r@gmail.com>
2021-03-19 16:23:37 +0600

    modified config vue

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,16 @@ const path = require('path');
 
 module.exports = {
 	transpileDependencies: ['vuetify'],
-
+	css: {
+		extract: process.env.NODE_ENV === 'production' ? {ignoreOrder: true} : false,
+	},
+	configureWebpack: {
+		performance: {
+			hints: process.env.NODE_ENV === 'production' ? "warning" : false,
+			maxAssetSize: 700000,
+			maxEntrypointSize: 700000
+		},
+	},
 	pluginOptions: {
 		'style-resources-loader': {
 			preProcessor: 'scss',
