782ec27332452738e1458909de0ee38089c3d2e7
Ramazan <laumanov.r@gmail.com>
2021-03-24 16:46:45 +0600

    add delete icon

---
+++ src/components/icons/DeleteIcon.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="delete-icon" @click="$emit('click')">
+		<img src="../../assets/icons/trash.svg" title="Удалить" />
+	</div>
+</template>
+
+<style lang="scss" scoped>
+	.delete-icon {
+		display: inline-block;
+		cursor: pointer;
+		margin-right: 10px;
+		width: 26px;
+		min-width: 26px;
+		height: 26px;
+		img {
+			border-radius: 5px;
+			padding: 3px;
+			width: 100%;
+			height: 100%;
+		}
+	}
+</style>
