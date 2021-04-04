e760cf650975a09ad1d45281a7b489bb8f5567bb
Ramazan <laumanov.r@gmail.com>
2021-04-04 12:05:27 +0600

    change object fit in main

--- src/views/client/Main.vue
+++ src/views/client/Main.vue
@@ -629,7 +629,7 @@ export default {
 							img {
 								width: 100%;
 								height: 100%;
-								object-fit: fill;
+								object-fit: cover;
 								border-radius: 13px 0 0 13px;
 							}
 						}
