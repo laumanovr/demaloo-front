5604b4f70ad3e30deefa208cfed5460d91fa2777
Ramazan <laumanov.r@gmail.com>
2021-04-07 16:34:02 +0600

    removed login page

--- src/router/index.js
+++ src/router/index.js
@@ -8,14 +8,14 @@ Vue.use(VueRouter);
 
 const routes = [
 	// CLIENT LOGIN AND REGISTRATION
-	{
-		path: '/login',
-		name: 'login',
-		component: loadComponent('login/ClientLogin'),
-		meta: {
-			requireAuth: false
-		}
-	},
+	// {
+	// 	path: '/login',
+	// 	name: 'login',
+	// 	component: loadComponent('login/ClientLogin'),
+	// 	meta: {
+	// 		requireAuth: false
+	// 	}
+	// },
 
 	// CLIENT SIDE
 	{
