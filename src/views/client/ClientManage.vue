ddfed2ee446bae7abde7048c30d4d3e38dd7d7b8
Ramazan <laumanov.r@gmail.com>
2021-04-14 16:23:23 +0600

    testing login modal

--- src/views/client/ClientManage.vue
+++ src/views/client/ClientManage.vue
@@ -5,14 +5,24 @@
 		<Footer/>
 		<!--LOGIN MODAL-->
 		<modal
-			transition="vm-transition--overlay-enter-active"
-			name="login-modal"
-			class="bor-radius"
+			name="login-modal" class="bor-radius"
 			:width="isMobileWindow ? '100%' : '1020px'"
 			:height="isMobileWindow ? '100%' : '508px'"
 		>
 			<CloseIcon @click="toggleLoginModal" class="mob"/>
-			<ClientLogin/>
+			<div class="client-sign-up-in-container">
+				<div class="login-image web">
+					<img src="../../assets/images/welcome.png" class="welcome">
+					<div class="logo-text">
+						<img src="../../assets/images/big-logo.svg">
+						<div class="title">
+							<img src="../../assets/images/demaloo-text.png">
+							<span>Отдых без забот</span>
+						</div>
+					</div>
+				</div>
+				<ClientLogin/>
+			</div>
 		</modal>
 	</div>
 </template>
