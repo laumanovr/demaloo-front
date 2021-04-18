40fe9abbaf36472ea50d6c2b0ac8666073e930a2
Ramazan <laumanov.r@gmail.com>
2021-04-18 13:53:45 +0600

    modified location service

--- src/services/location.service.js
+++ src/services/location.service.js
@@ -8,8 +8,8 @@ export class LocationService {
 		return sendGetRequest(url);
 	}
 
-	static addNewPlace(data, regionId, rayonId) {
-		const url = `${API_BASE_URL}/places/${regionId}/${rayonId}/addPlace`;
+	static addNewPlace(data, regionId) {
+		const url = `${API_BASE_URL}/places/${regionId}/addPlace`;
 		return sendPatchRequest(url, data);
 	}
 
