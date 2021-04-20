cf40a6c942975f1e7463dc47b890bfd775a70935
Ramazan <laumanov.r@gmail.com>
2021-04-20 17:07:26 +0600

    include booking module store

--- src/store/index.js
+++ src/store/index.js
@@ -4,6 +4,7 @@ import {account} from './account.module';
 import {location} from './location.module';
 import {notification} from './notification.module';
 import {team} from './team.module';
+import {booking} from './booking.module';
 
 Vue.use(Vuex);
 
@@ -12,6 +13,7 @@ export default new Vuex.Store({
 		account,
 		location,
 		notification,
-		team
+		team,
+		booking
 	},
 });
