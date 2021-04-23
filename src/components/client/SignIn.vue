95fdc47efee9269e7cbd769f0de52a597d0c8985
Ramazan <laumanov.r@gmail.com>
2021-04-23 15:26:31 +0600

    check phone valid on login

--- src/components/client/SignIn.vue
+++ src/components/client/SignIn.vue
@@ -2,17 +2,15 @@
 	<div class="client-login">
 		<h3 class="head-title">Войти в учетную запись</h3>
 		<v-form ref="signInForm">
-			<div class="masked-input" @click="isDisabled = false">
+			<div class="masked-input">
 				<span class="label">Телефон</span>
 				<MaskedInput
 					mask="\+\996 (111) 11-11-11"
 					placeholder="+996(555)12-34-56"
 					@input="phoneValue = arguments[1]"
-					:readonly="isDisabled"
 				/>
-				<v-text-field class="error-only" v-model="phoneValue" :rules="phoneRule"/>
 			</div>
-			<div class="password">
+			<div class="password" @click="isDisabled=false">
 				<span class="label">Пароль</span>
 				<v-text-field
 					solo
@@ -44,10 +42,10 @@ export default {
 	data() {
 		return {
 			requiredRule: [(v) => !!v || 'Обязательное поле'],
-			phoneRule: [
-				v => !!v || 'Обязательное поле',
-				v => ( v && !v.includes('_') ) || 'Введите правильный номер телефона'
-			],
+			// phoneRule: [
+			// 	v => !!v || 'Обязательное поле',
+			// 	v => ( v && !v.includes('_') ) || 'Введите правильный номер телефона'
+			// ],
 			phoneValue: '',
 			loginObj: {
 				phoneNumber: '',
@@ -58,6 +56,10 @@ export default {
 	},
 	methods: {
 		submitLogin() {
+			if (this.phoneValue.includes('_')) {
+				this.$toast.error('Введите правильный номер телефона');
+				return;
+			}
 			if (this.$refs.signInForm.validate()) {
 				this.$emit('loading', true);
 				this.loginObj.phoneNumber = `+996${this.phoneValue}`;
