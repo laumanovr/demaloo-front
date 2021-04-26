35dc799abbd615777d2eb9d5b2fe27de35be566a
Ramazan <laumanov.r@gmail.com>
2021-04-26 12:30:59 +0600

    add check notifies on reload in client

--- src/views/client/ClientManage.vue
+++ src/views/client/ClientManage.vue
@@ -11,6 +11,7 @@
 import Header from '@/components/general/Header';
 import Footer from '@/components/general/Footer';
 import LoginModal from '@/components/client/LoginModal';
+import {mapState} from 'vuex';
 
 export default {
 	components: {
@@ -18,9 +19,22 @@ export default {
 		Footer,
 		LoginModal
 	},
+	created() {
+		this.$store.dispatch('notification/checkClientNotifies');
+	},
+	computed: {
+		...mapState('notification', ['onNewNotifies'])
+	},
 	methods: {
 		openLoginModal() {
 			this.$modal.show('login-modal');
+		}
+	},
+	watch: {
+		onNewNotifies(newNotify) {
+			if (newNotify) {
+				this.$toast.info('У вас новые уведомления!', {duration: 3500});
+			}
 		}
 	}
 };
