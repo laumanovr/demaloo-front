349a8ac5dde8323f8ff281d81a41e64466ff8c30
Ramazan <laumanov.r@gmail.com>
2021-04-30 00:52:42 +0600

    create logout modal client

---
+++ src/components/client/LogoutModal.vue
@@ -0,0 +1,25 @@
+<template>
+	<modal name="logout-modal" width="320px" height="auto">
+		<div class="modal-container">
+			<h3>Выйти?</h3>
+			<div class="btn-actions">
+				<button class="btn red-primary" @click="closeModal">Отмена</button>
+				<button class="btn green-main" @click="logOut">Выход</button>
+			</div>
+		</div>
+	</modal>
+</template>
+
+<script>
+	export default {
+		methods: {
+			closeModal() {
+				this.$modal.hide('logout-modal');
+			},
+			logOut() {
+				this.$store.dispatch('account/logout');
+				this.closeModal();
+			}
+		}
+	}
+</script>
