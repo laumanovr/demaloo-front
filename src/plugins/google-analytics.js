31fea646de4e80735c1837cd428f47b846ba1647
Ramazan <laumanov.r@gmail.com>
2021-05-10 13:36:33 +0600

    create plugin vue google analytics

---
+++ src/plugins/google-analytics.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+import VueGtag from 'vue-gtag';
+import router from '../router';
+
+if (process.env.NODE_ENV === 'production') {
+	Vue.use(VueGtag, {
+		config: { id: 'G-F67TSNJPY9' }
+	}, router);
+}
