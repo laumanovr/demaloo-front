0ece3e54bde5e279204247e841fa337475ee7ac0
Ramazan <laumanov.r@gmail.com>
2021-06-04 13:00:50 +0600

    create separate mobile tour filter modal

---
+++ src/components/client/MobileTourFilter.vue
@@ -0,0 +1,187 @@
+<template>
+	<modal
+		name="filter-modal"
+		class="mobile-filter-container"
+		height="95%" :adaptive="true"
+		:shiftY="1" @before-open="setSomeData"
+	>
+		<div class="modal-container">
+			<div class="head flex align-center justify-space-between">
+				<span class="clear" @click="clearFilter">Очистить</span>
+				<span class="title">Фильтры</span>
+				<CloseIcon class="close" @click="toggleFilterModal"/>
+			</div>
+			<div class="filters">
+				<div class="filter-item">
+					<v-select
+						outlined
+						label="Тур компания"
+						class="no-detail"
+						:items="allCompanies"
+						item-text="name"
+						item-value="_id"
+						v-model="filter.sortCompany"
+					/>
+				</div>
+				<div class="filter-item">
+					<v-select
+						outlined
+						label="Длительность (дней)"
+						class="no-detail"
+						:items="allDurations"
+						item-text="name"
+						item-value="val"
+						v-model="filter.sortDuration"
+					/>
+				</div>
+				<div class="filter-item d-flex align-center justify-space-between">
+					<v-text-field
+						outlined
+						label="Цена От"
+						class="no-detail from"
+						type="number"
+						v-model="filter.sortPriceFrom"
+					/>
+					<v-text-field
+						outlined
+						label="Цена До"
+						class="no-detail to"
+						type="number"
+						v-model="filter.sortPriceTo"
+					/>
+				</div>
+				<div class="filter-item category">
+					<span class="label">Категории</span>
+					<div class="check-boxes">
+						<label class="box" v-for="(item, i) in allCategories" :key="i" :for="item.id+'-box'">
+							<input type="checkbox" v-model="item.checked" @change="chooseCategory($event, item)" :id="item.id+'-box'">
+							<span>{{item.ru}}</span>
+						</label>
+					</div>
+				</div>
+			</div>
+			<div class="d-flex justify-center submit-btn">
+				<button class="btn green-main" @click="submitFilter">Применить</button>
+			</div>
+		</div>
+	</modal>
+</template>
+
+<script>
+import CloseIcon from '@/components/icons/CloseIcon';
+
+export default {
+	props: {
+		sortItems: Array,
+		allCompanies: Array,
+		allDurations: Array,
+		categories: Array
+	},
+	components: {
+		CloseIcon
+	},
+	data() {
+		return {
+			filter: {
+				sortCompany: '',
+				sortDuration: '',
+				sortPriceFrom: '',
+				sortPriceTo: '',
+				categoriesQuery: '',
+				sortPage: 1
+			},
+			sortCategories: [],
+			allCategories: []
+		};
+	},
+	methods: {
+		setSomeData() {
+			if (!this.allCategories.length) {
+				this.allCategories = JSON.parse(JSON.stringify(this.categories));
+			}
+		},
+
+		toggleFilterModal() {
+			this.$modal.toggle('filter-modal');
+		},
+
+		chooseCategory(e, category) {
+			this.filter.categoriesQuery = '';
+			if (e.currentTarget.checked) {
+				this.sortCategories.push(category);
+			} else {
+				let index = this.sortCategories.findIndex((i) => i.id === category.id);
+				this.sortCategories.splice(index, 1);
+			}
+			this.sortCategories.forEach((category) => {
+				this.filter.categoriesQuery += `&categories[]=${category.id}`;
+			});
+		},
+
+		clearFilter() {
+			this.filter = {
+				sortCompany: '',
+				sortDuration: '',
+				sortPriceFrom: '',
+				sortPriceTo: '',
+				categoriesQuery: ''
+			};
+			this.sortCategories = [];
+			this.allCategories = this.allCategories.map((item) => ({...item, checked: false}));
+		},
+
+		submitFilter() {
+			this.filter.sortPage = 1;
+			this.$emit('onSubmit', this.filter);
+		}
+	}
+};
+</script>
+
+<style lang="scss">
+	.mobile-filter-container {
+		.vm--modal {
+			border-radius: 8px 8px 0 0;
+			color: $blue-darkest;
+		}
+		.head {
+			margin-bottom: 16px;
+			.clear {
+				font-size: 14px;
+				color: $green-main;
+				font-weight: 600;
+			}
+			.title {
+				font-size: 16px;
+				font-weight: 700;
+				font-family: $montserrat;
+			}
+		}
+		.filters {
+			.filter-item {
+				margin-bottom: 12px;
+				&.category {
+					.label {
+						font-size: 16px;
+						font-weight: 600;
+					}
+					.box {
+						display: block;
+						font-size: 16px;
+						font-weight: 400;
+						margin-top: 8px;
+						span {
+							margin-left: 14px;
+						}
+					}
+				}
+				.no-detail.from {
+					margin-right: 15px;
+				}
+			}
+		}
+		.submit-btn {
+			padding: 10px 0;
+		}
+	}
+</style>
