3d8d5565c1c35bb216940dc3a4951f8950c864bd
Ramazan <laumanov.r@gmail.com>
2021-06-21 15:34:44 +0600

    create client chat page

---
+++ src/views/client/ClientChat.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="client-chat-container">
+		<Chat/>
+	</div>
+</template>
+
+<script>
+import Chat from '@/components/general/Chat';
+
+export default {
+	components: {
+		Chat
+	}
+};
+</script>
