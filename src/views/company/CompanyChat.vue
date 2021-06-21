29da337facac0ef967e203644c9de4de138ee001
Ramazan <laumanov.r@gmail.com>
2021-06-21 15:35:01 +0600

    create company chat page

---
+++ src/views/company/CompanyChat.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="company-chat-container">
+		<Chat/>
+	</div>
+</template>
+
+<script>
+import Chat from '@/components/general/Chat';
+
+export default {
+	components: {
+		Chat
+	}
+};
+</script>
