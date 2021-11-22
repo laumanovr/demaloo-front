b3ff21027f3f36a8de52318f78cf1670f566c57c
Ramazan <laumanov.r@gmail.com>
2021-11-22 16:47:02 +0600

    create blog service

---
+++ src/services/blog.service.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+const MainUrl = 'https://demaloo.cdn.prismic.io/api/v2';
+
+export class BlogService {
+    constructor() {}
+
+    static async fetchAllBlogs(page = 1) {
+        try {
+            const latestRef = await this.fetchLatestRef();
+            const fetchUrl = `${MainUrl}/documents/search?ref=${latestRef}`;
+            const res = await axios.get(`${fetchUrl}&q=[[at(document.type, "blog")]]&page=${page}`);
+            return res.data;
+        } catch (err) {
+            return Promise.reject(err);
+        }
+    }
+
+    static async fetchById(id) {
+        try {
+            const latestRef = await this.fetchLatestRef();
+            const fetchUrl = `${MainUrl}/documents/search?ref=${latestRef}&q=[[in(document.id, ["${id}"])]]`;
+            const res = await axios.get(fetchUrl);
+            return res.data.results[0].data;
+        } catch (err) {
+            return Promise.reject(err);
+        }
+    }
+
+    static async fetchLatestRef() {
+        try {
+            const res = await axios.get(MainUrl);
+            return res.data.refs[0].ref;
+        } catch (err) {
+            return Promise.reject(err);
+        }
+    }
+}
